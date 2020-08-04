d9d8b8e95a0b567c3bc8e4871c3e7e7b470de6eb
Denis <black.wolf.31@mail.ru>
2020-08-04 15:58:22 +0300

    My first commit

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'status')
+    list_filter = ('title', 'slug', 'author', 'status')
+    prepopulated_fields = {'slug' : ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
