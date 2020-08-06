930f2186e7e8c69bc21b30a3549ad0f9a27eb909
Denis <black.wolf.31@mail.ru>
2020-08-06 15:38:54 +0300

    My commit

--- minecraft_block/settings.py
+++ minecraft_block/settings.py
@@ -13,8 +13,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'static'),
+    # Add to this list all the locations containing your static files
+)
 
 
 # Quick-start development settings - unsuitable for production
